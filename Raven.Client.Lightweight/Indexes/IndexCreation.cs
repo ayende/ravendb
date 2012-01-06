6929d78daa462ead0e34f31f42e4b818bd3e19f1
Daniel Lang <d.lang@flexbit.at>
2012-01-06 10:30:41 +0100

    added CreateIndexesForDatabases

--- Raven.Client.Lightweight/Indexes/IndexCreation.cs
+++ Raven.Client.Lightweight/Indexes/IndexCreation.cs
@@ -4,6 +4,7 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,6 +14,8 @@ using System.ComponentModel.Composition.Hosting;
 using Raven.Client.Connection;
 using Raven.Client.Connection.Async;
 using System.Threading.Tasks;
+using Raven.Client.Extensions;
+
 #endif
 
 namespace Raven.Client.Indexes
@@ -52,12 +55,43 @@ namespace Raven.Client.Indexes
 		/// <summary>
 		/// Creates the indexes found in the specified catalog
 		/// </summary>
-		/// <param name="catalogToGetnIndexingTasksFrom">The catalog to getn indexing tasks from.</param>
+		/// <param name="catalogToGetnIndexingTasksFrom">The catalog to get indexing tasks from.</param>
 		/// <param name="documentStore">The document store.</param>
 		public static void CreateIndexes(ExportProvider catalogToGetnIndexingTasksFrom, IDocumentStore documentStore)
 		{
 			CreateIndexes(catalogToGetnIndexingTasksFrom, documentStore.DatabaseCommands, documentStore.Conventions);
 		}
+
+        /// <summary>
+        /// Creates the indexes found in the specified catalog in multiple databases
+        /// </summary>
+        /// <param name="assemblyToScanForIndexingTasks">The catalog to get indexing tasks from.</param>
+        /// <param name="documentStore">The document store.</param>
+        /// <param name="databases">Identifiers of the databases on which the indexes should be created on</param>
+        /// <param name="shouldIgnoreNotExistingDatabase">Ignores not existing databases when set to true</param>
+        public static void CreateIndexesForDatabases(Assembly assemblyToScanForIndexingTasks, IDocumentStore documentStore, string[] databases, bool shouldIgnoreNotExistingDatabase = false)
+        {
+            var catalog = new CompositionContainer(new AssemblyCatalog(assemblyToScanForIndexingTasks));
+            foreach (var database in databases)
+            {
+                bool exists = true;
+                try
+                {
+                    documentStore.DatabaseCommands.EnsureDatabaseExists(database);
+                }
+                catch (Exception)
+                {
+                    if (!shouldIgnoreNotExistingDatabase)
+                        throw new InvalidOperationException(string.Format("Cannot create indexes for database '{0}' because it doesn't exist.", database));
+                    exists = false;
+                }
+
+                if (exists)
+                {
+                    CreateIndexes(catalog, documentStore.DatabaseCommands.ForDatabase(database), documentStore.Conventions);
+                }
+            }
+        }
 #endif
 
 		/// <summary>
