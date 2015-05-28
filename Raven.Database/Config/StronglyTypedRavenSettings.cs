60d309f013eda1d2518594985c80226ba03c5d30
aviviadi <avivi.adi.hr@gmail.com>
2015-05-28 20:19:59 +0300

    additional merge

--- Raven.Database/Config/StronglyTypedRavenSettings.cs
+++ Raven.Database/Config/StronglyTypedRavenSettings.cs
@@ -76,6 +76,9 @@ namespace Raven.Database.Config
 				// we allow 1 GB by default, or up to 75% of available memory on startup, if less than that is available
 				Math.Min(1024, (int)(MemoryStatistics.AvailableMemory * 0.75)));
 
+			LowMemoryLimitForLinuxDetectionInMB = 
+				new IntegerSetting(settings[Constants.LowMemoryLimitForLinuxDetectionInMB],
+					Math.Min(16, (int)(MemoryStatistics.AvailableMemory * 0.10))); // AvailableMemory reports in MB
 			MaxPageSize =
 				new IntegerSettingWithMin(settings["Raven/MaxPageSize"], 1024, 10);
 			MemoryCacheLimitMegabytes =
