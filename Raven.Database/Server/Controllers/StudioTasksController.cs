95d350b92a366383b88c782dea84243bd49c60ec
aviviadi <avivi.adi.hr@gmail.com>
2015-06-02 19:10:43 +0300

    MERGE:RavenTextFieldParser (avoid VisualBasic dependency). Not compileable

--- Raven.Database/Server/Controllers/StudioTasksController.cs
+++ Raven.Database/Server/Controllers/StudioTasksController.cs
@@ -18,7 +18,7 @@ using System.Web.Http;
 using Jint;
 using Jint.Parser;
 
-using Microsoft.VisualBasic.FileIO;
+// using Microsoft.VisualBasic.FileIO;
 using Raven.Imports.Newtonsoft.Json;
 using Raven.Abstractions;
 using Raven.Abstractions.Commands;
