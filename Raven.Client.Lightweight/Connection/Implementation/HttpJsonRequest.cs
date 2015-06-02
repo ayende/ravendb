39486262ef628b086eba8e4d4d9740cc812e49d9
aviviadi <avivi.adi.hr@gmail.com>
2015-06-02 15:43:53 +0300

    transferEncodingChunked = false when runningOnMono

--- Raven.Client.Lightweight/Connection/Implementation/HttpJsonRequest.cs
+++ Raven.Client.Lightweight/Connection/Implementation/HttpJsonRequest.cs
@@ -680,7 +680,7 @@ namespace Raven.Client.Connection.Implementation
 		        Content = new JsonContent(tokenToWrite),
 		        Headers =
 		        {
-			        TransferEncodingChunked = true
+					TransferEncodingChunked = !EnvironmentUtils.RunningOnMono
 		        }
 	        });
         }
@@ -705,7 +705,7 @@ namespace Raven.Client.Connection.Implementation
 				Content = content,
 				Headers =
 				{
-					TransferEncodingChunked = true,
+					TransferEncodingChunked = !EnvironmentUtils.RunningOnMono,
 				}
 			});
 		}
@@ -765,7 +765,7 @@ namespace Raven.Client.Connection.Implementation
 
 		public async Task<HttpResponseMessage> ExecuteRawRequestAsync(Action<Stream, TaskCompletionSource<object>> action)
 		{
-			httpClient.DefaultRequestHeaders.TransferEncodingChunked = true;
+			httpClient.DefaultRequestHeaders.TransferEncodingChunked = !EnvironmentUtils.RunningOnMono;
 
             return await RunWithAuthRetry(async () =>
             {
