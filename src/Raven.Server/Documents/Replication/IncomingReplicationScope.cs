5c861a9d2a00d517d03113a5a02e532993949629
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-07-25 14:02:14 +0300

    added forgotten file

---
+++ src/Raven.Server/Documents/Replication/IncomingReplicationScope.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Raven.Server
+{
+    public class IncomingReplicationScope : IDisposable
+    {
+        [ThreadStatic]
+        public static bool IsActive;
+
+        public IncomingReplicationScope()
+        {
+            IsActive = true;
+        }
+
+        public void Dispose()
+        {
+            IsActive = false;
+        }
+    }
+}
