30444c99b8b1c9b744725ae3e865ab4786c7a545
talweiss1982 <tal@ayende.com>
2016-08-22 15:44:46 +0300

    RavenDB-4713 Fixing DNX issue in grisha's test

--- Raven.Tests.Core/Querying/escaping.cs
+++ Raven.Tests.Core/Querying/escaping.cs
@@ -10,7 +10,7 @@ namespace Raven.Tests.Core.Querying
     public class Escaping : RavenCoreTestBase
     {
 #if DNXCORE50
-        public Intersection(TestServerFixture fixture)
+        public Escaping(TestServerFixture fixture)
             : base(fixture)
         {
 
