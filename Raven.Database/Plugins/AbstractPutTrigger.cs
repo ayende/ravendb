7dbab29ea7ec706a6af0a6cc183c0d98e88a9209
Tal weiss <tal@ayende.com>
2016-08-28 10:56:14 +0300

    RavenDB-5176 changing the name of a parameter in AbstractPutTrigger so it will make sense

--- Raven.Database/Plugins/AbstractPutTrigger.cs
+++ Raven.Database/Plugins/AbstractPutTrigger.cs
@@ -44,7 +44,7 @@ namespace Raven.Database.Plugins
         ///  Any call to the provided <seealso cref="DocumentDatabase" /> instance will be done under the
         ///  same transaction as the PUT operation.
         ///  </remarks><param name="key">The document key</param><param name="jsonReplicationDocument">The new document about to be put into Raven</param><param name="metadata">The new document metadata</param><param name="transactionInformation">The current transaction, if it exists</param>
-        public virtual void OnPut(string key, RavenJObject jsonReplicationDocument, RavenJObject metadata, TransactionInformation transactionInformation)
+        public virtual void OnPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
         {
             
         }
