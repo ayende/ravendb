700ecec9c08328d870067277f66ef87a662bbfe4
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-09-19 14:14:50 +0300

    fixing whitespace (RavenDB-5250)

--- test/FastTests/Server/Documents/Replication/ReplicationDocuments.cs
+++ test/FastTests/Server/Documents/Replication/ReplicationDocuments.cs
@@ -3,7 +3,7 @@
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
-
+ 
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,98 +15,98 @@ using Xunit;
 
 namespace FastTests.Server.Documents.Replication
 {
-	public class DocumentReplication : ReplicationTestsBase
-	{
-		[Fact]
-		public async Task CanReplicateDocument()
-		{
-			using (var source = GetDocumentStore())
-			using (var destination = GetDocumentStore())
-			{
-				SetupReplication(source, destination);
+    public class DocumentReplication : ReplicationTestsBase
+    {
+        [Fact]
+        public async Task CanReplicateDocument()
+        {
+            using (var source = GetDocumentStore())
+            using (var destination = GetDocumentStore())
+            {
+                SetupReplication(source, destination);
 
-				using (var session = source.OpenAsyncSession())
-				{
-					var user = new User { Name = "Arek" };
+                using (var session = source.OpenAsyncSession())
+                {
+                    var user = new User { Name = "Arek" };
 
-					await session.StoreAsync(user);
+                    await session.StoreAsync(user);
 
-					await session.SaveChangesAsync();
+                    await session.SaveChangesAsync();
 
-					await source.Replication.WaitAsync(etag: session.Advanced.GetEtagFor(user));
-				}
+                    await source.Replication.WaitAsync(etag: session.Advanced.GetEtagFor(user));
+                }
 
-				using (var session = destination.OpenAsyncSession())
-				{
-					var user = await session.LoadAsync<User>("users/1");
+                using (var session = destination.OpenAsyncSession())
+                {
+                    var user = await session.LoadAsync<User>("users/1");
 
-					Assert.NotNull(user);
-					Assert.Equal("Arek", user.Name);
-				}
-			}
-		}
+                    Assert.NotNull(user);
+                    Assert.Equal("Arek", user.Name);
+                }
+            }
+        }
 
-		[Fact]
-		public void CanReplicateDocumentDeletion()
-		{
-			using (var source = GetDocumentStore())
-			using (var destination = GetDocumentStore())
-			{
-				SetupReplication(source, destination);
+        [Fact]
+        public void CanReplicateDocumentDeletion()
+        {
+            using (var source = GetDocumentStore())
+            using (var destination = GetDocumentStore())
+            {
+                SetupReplication(source, destination);
 
-				source.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
+                source.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
 
-				var document = WaitForDocument(destination, "docs/1");
+                var document = WaitForDocument(destination, "docs/1");
 
-				Assert.NotNull(document);
+                Assert.NotNull(document);
 
-				source.DatabaseCommands.Delete("docs/1", null);
+                source.DatabaseCommands.Delete("docs/1", null);
 
-				for (int i = 0; i < 10; i++)
-				{
-					if (destination.DatabaseCommands.Get("docs/1") == null)
-						break;
-					Thread.Sleep(100);
-				}
+                for (int i = 0; i < 10; i++)
+                {
+                    if (destination.DatabaseCommands.Get("docs/1") == null)
+                        break;
+                    Thread.Sleep(100);
+                }
 
-				Assert.Null(destination.DatabaseCommands.Get("docs/1"));
-			}
-		}
+                Assert.Null(destination.DatabaseCommands.Get("docs/1"));
+            }
+        }
 
-		[Fact(Skip = "Client API needs to support changed protocol of replication conflicts before this would work")]
-		public async Task ShouldCreateConflictThenResolveIt()
-		{
-			using (var source = GetDocumentStore())
-			using (var destination = GetDocumentStore())
-			{
-				source.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
-				destination.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value2" } }, new RavenJObject());
+        [Fact(Skip = "Client API needs to support changed protocol of replication conflicts before this would work")]
+        public async Task ShouldCreateConflictThenResolveIt()
+        {
+            using (var source = GetDocumentStore())
+            using (var destination = GetDocumentStore())
+            {
+                source.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
+                destination.DatabaseCommands.Put("docs/1", null, new RavenJObject() { { "Key", "Value2" } }, new RavenJObject());
 
-				SetupReplication(source, destination);
+                SetupReplication(source, destination);
 
-				source.DatabaseCommands.Put("marker", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
+                source.DatabaseCommands.Put("marker", null, new RavenJObject() { { "Key", "Value" } }, new RavenJObject());
 
-				var marker = WaitForDocument(destination, "marker");
+                var marker = WaitForDocument(destination, "marker");
 
-				Assert.NotNull(marker);
+                Assert.NotNull(marker);
 
-				var conflicts = await GetConflicts(destination, "docs/1");
-				Assert.Equal(2,conflicts["docs/1"].Count);
-				Assert.NotEqual(conflicts["docs/1"][0][0].DbId, conflicts["docs/1"][1][0].DbId);
-				Assert.Equal(1,conflicts["docs/1"][0][0].Etag);
-				Assert.Equal(1, conflicts["docs/1"][1][0].Etag);
+                var conflicts = await GetConflicts(destination, "docs/1");
+                Assert.Equal(2,conflicts["docs/1"].Count);
+                Assert.NotEqual(conflicts["docs/1"][0][0].DbId, conflicts["docs/1"][1][0].DbId);
+                Assert.Equal(1,conflicts["docs/1"][0][0].Etag);
+                Assert.Equal(1, conflicts["docs/1"][1][0].Etag);
 
-				var conflictException = Assert.Throws<ConflictException>(() => destination.DatabaseCommands.Get("docs/1"));
-				Assert.Equal("Conflict detected on docs/1, conflict must be resolved before the document will be accessible", conflictException.Message);
+                var conflictException = Assert.Throws<ConflictException>(() => destination.DatabaseCommands.Get("docs/1"));
+                Assert.Equal("Conflict detected on docs/1, conflict must be resolved before the document will be accessible", conflictException.Message);
 
-				// resolve by using first
-				//TODO : when client API is finished, refactor this so the test works as designed
-				//var resolution = destination.DatabaseCommands.Get(conflictException.ConflictedVersionIds[0]);
-				//resolution.Metadata.Remove(Constants.Replication.RavenReplicationConflictDocument);
-				//destination.DatabaseCommands.Put("docs/1", null, resolution.DataAsJson, resolution.Metadata);
+                // resolve by using first
+                //TODO : when client API is finished, refactor this so the test works as designed
+                //var resolution = destination.DatabaseCommands.Get(conflictException.ConflictedVersionIds[0]);
+                //resolution.Metadata.Remove(Constants.Replication.RavenReplicationConflictDocument);
+                //destination.DatabaseCommands.Put("docs/1", null, resolution.DataAsJson, resolution.Metadata);
 
-				//destination.DatabaseCommands.GetAttachment("docs/1");
-			}
-		}
-	}
+                //destination.DatabaseCommands.GetAttachment("docs/1");
+            }
+        }
+    }
 }
