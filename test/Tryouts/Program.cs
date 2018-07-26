5a98b2066242304dcc7ca63af4fd7c3decca55f9
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-07-26 20:07:35 +0300

    Graph query AST WIP

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,36 +1,10 @@
-using System;
-using System.Threading.Tasks;
-using FastTests;
-using FastTests.Server.Documents.Queries.Parser;
-using FastTests.Voron.Backups;
-using FastTests.Voron.Compaction;
-using RachisTests.DatabaseCluster;
-using Raven.Client.Documents.Queries;
-using Raven.Tests.Core.Utils.Entities;
-using SlowTests.Authentication;
-using SlowTests.Bugs.MapRedue;
-using SlowTests.Client;
-using SlowTests.Client.Attachments;
-using SlowTests.Issues;
-using SlowTests.MailingList;
-using Sparrow.Logging;
-using StressTests.Client.Attachments;
-using Xunit;
-
 namespace Tryouts
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            for (int i = 0; i < 1000; i++)
-            {
-                Console.WriteLine(i);
-                using (var test = new RavenDB_7043())
-                {
-                    test.Should_mark_index_as_errored_and_throw_on_querying_it_even_its_very_small_and_everything_fails();
-                }               
-            }
+
         }
     }
 }
