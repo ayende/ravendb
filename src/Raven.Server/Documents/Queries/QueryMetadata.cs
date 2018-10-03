dbccfc5a134020fdef98e45e6fc867ecec45e8bd
egor <egor@ravendb.net>
2018-10-03 11:25:39 +0300

    RavenDB-12019: Validation moved to QueryMetadata.

--- src/Raven.Server/Documents/Queries/QueryMetadata.cs
+++ src/Raven.Server/Documents/Queries/QueryMetadata.cs
@@ -85,6 +85,9 @@ namespace Raven.Server.Documents.Queries
             }
 
 
+            if (IsGroupBy && IsCollectionQuery == false)
+                throw new ArgumentException("Can't use 'group by' when querying on an Index. 'group by' can be used only when querying on collections.");
+
             DeclaredFunctions = Query.DeclaredFunctions;
 
             Build(parameters);
