9b0f92047e91288d75b37f6d48b9929a12801150
Ayende Rahien <Ayende@ayende.com>
2020-10-08 14:40:52 +0300

    RavenDB-15690 IAsyncDocumentSession.HasChanges is false after deleting a record

---
+++ test/SlowTests/Issues/RavenDB-15690.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_15690 : RavenTestBase
+    {
+        public RavenDB_15690(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task CanGetHasChangesOnDelete()
+        {
+            using var store = GetDocumentStore();
+
+            using (var session = store.OpenAsyncSession())
+            {
+                await session.StoreAsync(new {Name = "Ayende"}, "users/ayende");
+                await session.SaveChangesAsync();
+            }
+            
+            using (var session = store.OpenAsyncSession())
+            {
+                var item = await session.LoadAsync<object>("users/ayende");
+                session.Delete(item);
+                Assert.True(session.Advanced.HasChanges);
+            }
+        }
+    }
+}
