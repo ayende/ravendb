ebcb8960d8fdb516b09f319a9d02e2a6860fb4df
Ayende Rahien <Ayende@ayende.com>
2024-12-11 15:51:43 +0200

    RavenDB-23085 - Removed journal recycling feature - because we want to use shared journals, and those two are in conflict with one another

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -756,7 +756,6 @@ namespace Raven.Server.Documents.Indexes
             options.IgnoreInvalidJournalErrors = documentDatabase.Configuration.Storage.IgnoreInvalidJournalErrors;
             options.SkipChecksumValidationOnDatabaseLoading = documentDatabase.Configuration.Storage.SkipChecksumValidationOnDatabaseLoading;
             options.IgnoreDataIntegrityErrorsOfAlreadySyncedTransactions = documentDatabase.Configuration.Storage.IgnoreDataIntegrityErrorsOfAlreadySyncedTransactions;
-            options.MaxNumberOfRecyclableJournals = documentDatabase.Configuration.Storage.MaxNumberOfRecyclableJournals;
             options.DisableSparseRegions = documentDatabase.Configuration.Storage.DisableSparseRegions;
 
             if (documentDatabase.ServerStore.GlobalIndexingScratchSpaceMonitor != null)
@@ -1890,7 +1889,7 @@ namespace Raven.Server.Documents.Indexes
                                 if (_logsAppliedEvent.Wait(Configuration.MaxTimeToWaitAfterFlushAndSyncWhenExceedingScratchSpaceLimit.AsTimeSpan))
                                 {
                                     // we've just flushed let's cleanup scratch space immediately
-                                    storageEnvironment.CleanupMappedMemory();
+                                    storageEnvironment.Cleanup();
                                 }
                             }
 
@@ -1989,7 +1988,7 @@ namespace Raven.Server.Documents.Indexes
 
 
                         if (totalSizeOfJournals >= Configuration.MinimumTotalSizeOfJournalsToRunFlushAndSyncWhenReplacingSideBySideIndex)
-                            FlushAndSync(_environment, (int)Configuration.MaxTimeToWaitAfterFlushAndSyncWhenReplacingSideBySideIndex.AsTimeSpan.TotalMilliseconds, tryCleanupRecycledJournals: true);
+                            FlushAndSync(_environment, (int)Configuration.MaxTimeToWaitAfterFlushAndSyncWhenReplacingSideBySideIndex.AsTimeSpan.TotalMilliseconds);
 
                         // this side-by-side index will be replaced in a second, notify about indexing success
                         // so we know that indexing batch is no longer in progress
@@ -2267,7 +2266,7 @@ namespace Raven.Server.Documents.Indexes
                                        $"going to try flushing and syncing the environment to cleanup the storage. " +
                                        $"Will wait for flush for: {timeToWaitInMilliseconds}ms", dfe);
 
-                FlushAndSync(storageEnvironment, timeToWaitInMilliseconds, true);
+                FlushAndSync(storageEnvironment, timeToWaitInMilliseconds);
                 return;
             }
 
@@ -2277,11 +2276,10 @@ namespace Raven.Server.Documents.Indexes
             if (State == IndexState.Error)
                 return;
 
-            storageEnvironment.Options.TryCleanupRecycledJournals();
             SetErrorState($"State was changed due to excessive number of disk full errors ({diskFullErrors}).");
         }
 
-        private void FlushAndSync(StorageEnvironment storageEnvironment, int timeToWaitInMilliseconds, bool tryCleanupRecycledJournals)
+        private void FlushAndSync(StorageEnvironment storageEnvironment, int timeToWaitInMilliseconds)
         {
             try
             {
@@ -2309,7 +2307,7 @@ namespace Raven.Server.Documents.Indexes
                 return;
             }
 
-            storageEnvironment.Cleanup(tryCleanupRecycledJournals);
+            storageEnvironment.Cleanup();
         }
 
         private void SetErrorState(string reason)
@@ -2342,7 +2340,7 @@ namespace Raven.Server.Documents.Indexes
                                            $"going to try flushing and syncing the environment to cleanup the scratch buffers. " +
                                            $"Will wait for flush for: {timeToWaitInMilliseconds}ms", exception);
 
-                    FlushAndSync(storageEnvironment, timeToWaitInMilliseconds, false);
+                    FlushAndSync(storageEnvironment, timeToWaitInMilliseconds);
                 }
 
                 if (_logger.IsInfoEnabled)
