43e2936c0d9faaf8b61403e0b20248e651b32f8f
Ayende Rahien <ayende@ayende.com>
2026-05-03 19:50:34 +0300

    RavenDB-25281 - Reverting docs entires (shouldn't be here). Removing benchmark code that isn't useful, since we'll want to bench all of Corax / RavenDB for this.

--- bench/Voron.Benchmark/Program.cs
+++ bench/Voron.Benchmark/Program.cs
@@ -1,57 +1,14 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
-using Voron.Benchmark.Corax;
 
 namespace Voron.Benchmark
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            if (args.Length > 0 && args[0] == "quick-roaring")
-            {
-                RoaringBitmapQuickBench.Run();
-                return;
-            }
-
-            if (args.Length > 0 && args[0] == "profile-roaring")
-            {
-                RoaringBitmapProfileBench.Run();
-                return;
-            }
-
-            if (args.Length > 0 && args[0] == "trace-roaring")
-            {
-                RoaringBitmapTraceBench.Run();
-                return;
-            }
-
-            if (args.Length > 0 && args[0] == "corax-roaring")
-            {
-                RoaringBitmapCoraxBench.Run();
-                return;
-            }
-
-            // Old corax-query benchmark removed — use corax-full-bench instead
-
-            if (args.Length > 0 && args[0] == "corax-full-bench")
-            {
-                var outputFile = args.Length > 1 ? args[1] : "/tmp/corax-full-bench-results.txt";
-                CoraxFullBenchmark.Run(outputFile);
-                return;
-            }
-
-            if (args.Length > 0 && args[0] == "contains-bench")
-            {
-                ContainsArrayBench.Run();
-                return;
-            }
-
 #if DEBUG
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
 #else
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 #endif
-        }
     }
 }
