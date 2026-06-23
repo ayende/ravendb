5c69ba70f79f9b1a46d114ec09293cc6e082a1d7
Ayende Rahien <ayende@ayende.com>
2026-06-23 21:59:15 +0300

    RavenDB-25281 - simpler code

--- src/Corax/Querying/Spatials/IndexSearcher.Spatial.cs
+++ src/Corax/Querying/Spatials/IndexSearcher.Spatial.cs
@@ -12,14 +12,9 @@ public partial class IndexSearcher
     public IQueryMatch SpatialQuery(in FieldMetadata field, double error, IShape shape, SpatialContext spatialContext, Utils.Spatial.SpatialRelation spatialRelation, in CancellationToken token = default)
     {
         var terms = _fieldsTree?.CompactTreeFor(field.FieldName);
-        if (terms == null)
-        {
-            // If either the term or the field does not exist the request will be empty.
+        if (terms == null)// If either the term or the field does not exist the request will be empty.
             return EmptyQueryMatch.Instance;
-        }
 
-        // Negation (NOT spatial.within(...)) is applied by the query pipeline's AndNot against the positive
-        // match, so this only ever builds the positive spatial post-filter.
         return field.HasBoost
             ? new SpatialMatch<HasBoosting>(this, _transaction.Allocator, spatialContext, field, shape, terms, error, spatialRelation, token)
             : new SpatialMatch<NoBoosting>(this, _transaction.Allocator, spatialContext, field, shape, terms, error, spatialRelation, token);
