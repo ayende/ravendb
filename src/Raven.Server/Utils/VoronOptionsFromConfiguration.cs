43d3d37984dcaa700dc35069d5ba9b0171cff4dc
Ayende Rahien <Ayende@ayende.com>
2026-08-23 03:07:27 +0000

    probe: preallocate 8GB data file

--- src/Raven.Server/Utils/VoronOptionsFromConfiguration.cs
+++ src/Raven.Server/Utils/VoronOptionsFromConfiguration.cs
@@ -9,6 +9,7 @@ namespace Raven.Server.Utils
         public static void Apply(StorageEnvironmentOptions options, RavenConfiguration configuration)
         {
             options.InitialLogFileSize = 128 * 1024 * 1024;
+            options.InitialFileSize = 8L * 1024 * 1024 * 1024;
 
             // Must be assigned first: this setter overwrites MaxLogFileSize, MaxScratchBufferSize and
             // MaxNumberOfPagesInJournalBeforeFlush, and while it is on, the MaxScratchBufferSize setter
